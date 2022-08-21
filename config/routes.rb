1da51b9fce6e1a3cff7a6c9d6920e5e5e7a74f11
Quill Cronwall <quilljazz@gmail.com>
2022-08-21 16:54:07 -0700

    Add show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users, only: [:show]
     end
   end
 end
