8af60fe5300bacfe8ea0271e14ea9876d0214540
Quill Cronwall <quilljazz@gmail.com>
2022-08-21 16:43:32 -0700

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # API definition
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+  end
 end
