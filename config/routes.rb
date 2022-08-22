e8475832769f44861d9556c4a5526d6f47c34948
Quill Cronwall <quilljazz@gmail.com>
2022-08-21 17:04:48 -0700

    Add destory action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create update]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
