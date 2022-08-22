db45776e4f4ef30fb377d90d969e8b9393d9ddc0
Quill Cronwall <quilljazz@gmail.com>
2022-08-21 20:19:33 -0700

    Add product serializer for custom JSON output

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -14,7 +14,7 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     get api_v1_product_url(@product), as: :json
     assert_response :success
     json_response = JSON.parse(response.body)
-    assert_equal @product.title, json_response['title']
+    assert_equal @product.title, json_response['data']['attributes']['title']
   end
 
   test 'should create product' do
@@ -43,25 +43,27 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should forbid update product' do
     patch api_v1_product_url(@product), params: { product: { title: @product.title } }, headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id) },
-          as: :json
+                                        as: :json
     assert_response :forbidden
   end
 
-  test "should destroy product" do
-assert_difference('Product.count', -1) do
-delete api_v1_product_url(@product), headers: {
-Authorization: JsonWebToken.encode(user_id: @product.user_id) },
-as: :json
-end
-assert_response :no_content
-end
+  test 'should destroy product' do
+    assert_difference('Product.count', -1) do
+      delete api_v1_product_url(@product), headers: {
+        Authorization: JsonWebToken.encode(user_id: @product.user_id)
+      },
+                                           as: :json
+    end
+    assert_response :no_content
+  end
 
-test "should forbid destroy product" do
-assert_no_difference('Product.count') do
-delete api_v1_product_url(@product), headers: {
-Authorization: JsonWebToken.encode(user_id: users(:two).id) },
-as: :json
-end
-assert_response :forbidden
-end
+  test 'should forbid destroy product' do
+    assert_no_difference('Product.count') do
+      delete api_v1_product_url(@product), headers: {
+        Authorization: JsonWebToken.encode(user_id: users(:two).id)
+      },
+                                           as: :json
+    end
+    assert_response :forbidden
+  end
 end
