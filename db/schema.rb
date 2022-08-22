e2d09964595c0ae69eefe61f97070cb6912293e8
Quill Cronwall <quilljazz@gmail.com>
2022-08-21 21:13:57 -0700

    Generate orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_003650) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_041144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -32,5 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_003650) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
